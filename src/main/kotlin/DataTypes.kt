0ff4a93aba01485667c58810572e95a8baad5b13
javaTpoint <javatpoint@xyz>
2023-08-10 09:03:46 +0300

    simple calculator

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+
+    var age:Int= 23// integer
+    var country:String= "kenya" //string
+    var letter:Char = 'k' //char
+    var myBoolean:Boolean = true
+    var temperature:Double = 37.5 //double
+    var points:Float = 40.34533323F //Float
+}
---
+++ src/main/kotlin/simplecalculator.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    println("enter first numbers")
+     var num1:Int = readLine()!!.toInt()
+    println("enter second numbers")
+     var num2:Int = readLine()!!.toInt()
+
+    var results = num1 +num2
+    print(results)
+
+}
