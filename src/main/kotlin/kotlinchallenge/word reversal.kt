cf173997127e9a8a906ac34ddecf1a231358e4a5
javaTpoint <javatpoint@xyz>
2023-08-18 10:19:50 +0300

    wordreverse

--- src/main/kotlin/kotlinchallenge/word reversal.kt
+++ src/main/kotlin/kotlinchallenge/word reversal.kt
@@ -2,7 +2,10 @@ package kotlinchallenge
 
 //1.Word Reversal: Create a program that takes a sentence as input and prints the words in reverse order.
 fun main(args: Array<String>) {
-    var str = "i love kenya"
-    var reverse = str.reversed()
-    println(reverse)
+
+    println("enter any word")
+    var words = readLine()?:""
+    var wordsplit = words.split(" ")
+    var wordsreverse=wordsplit.reversed()
+    println(wordsreverse.joinToString (" "))
 }
